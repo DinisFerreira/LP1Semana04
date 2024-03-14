87fda2ab9b6a13cdb258814350aad7ab8395bc61
Dinis <dinisvferreira@gmail.com>
2024-03-14 16:29:55 +0000

    add método Decor

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+  static void Main(string[] args)
+    {
+        string decoratedString = Decor("Hello", '*', 3);
+        Console.WriteLine(decoratedString); 
+    }
+
+    static string Decor(string input, char decorator, int repetitions)
+    {
+        string decoration = new string(decorator, repetitions);
+        return decoration + input + decoration;
+    }
+    }
+}
